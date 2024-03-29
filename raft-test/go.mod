afed461df4063c4694f697465655e7fa5cd94d86
Robert Oleynik <robert.oleynik@proton.me>
2024-03-29 16:16:44 +0100

    chore: remove legacy test application

--- raft-test/go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/robert-oleynik/k8s-playground/raft-test
-
-go 1.22.1
-
-require go.uber.org/zap v1.27.0
-
-require go.uber.org/multierr v1.10.0 // indirect
--- raft-test/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tmux split-window -h 'IDX=0 go run .; echo "Press Enter to exit"; read'
-sleep 1
-tmux split-window -v 'IDX=1 go run .; echo "Press Enter to exit"; read'
-sleep 1
-tmux split-window -v 'IDX=2 go run .; echo "Press Enter to exit"; read'
